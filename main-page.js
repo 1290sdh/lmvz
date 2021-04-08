7fbf71dfb444d6ba78aaf76a0e6fa8efb8a51db6
1290sdh <82150749+1290sdh@users.noreply.github.com>
2021-04-08 17:05:49 +0200

    Update and rename main.js to main-page.js

---
+++ main-page.js
@@ -0,0 +1,4 @@
+window.onload = function ...
+  if($_SERVER['HTTP_REFERER'] == "https://lmvz-digitall.xyz/login-page.html"){
+  } else {
+      window.location.replace("https://lmvz-digitall.xyz/login-page.html");
--- main.js
+++
@@ -1,4 +0,0 @@
-if($_SERVER['HTTP_REFERER'] == "https://lmvz-digitall.xyz/login-page.html"){
-  ...
-} else {
-    window.location.replace("https://lmvz-digitall.xyz/login-page.html");
