f86b3ba20963977f8b6b979eb1edc6a4acf9759a
1290sdh <82150749+1290sdh@users.noreply.github.com>
2021-04-08 16:20:47 +0200

    Update login-page.js

--- login-page.js
+++ login-page.js
@@ -13,6 +13,7 @@ loginButton.addEventListener("click", (e) => {
     if (username === "user" && password === "123456789") {
         // If the credentials are valid, show an alert box and reload the page
         alert("You have successfully logged in.");
+        show_image('http://google.com/images/logo.gif');
     } else {
         // Otherwise, make the login error message show (change its oppacity)
         loginErrorMsg.style.opacity = 1;
