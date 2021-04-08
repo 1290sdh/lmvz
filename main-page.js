040da137c8c60c6d7785da94fb31cc32363e9b00
1290sdh <82150749+1290sdh@users.noreply.github.com>
2021-04-08 17:17:52 +0200

    Delete main-page.js

--- main-page.js
+++
@@ -1,4 +0,0 @@
-window.onload = function ...
-  if($_SERVER['HTTP_REFERER'] == "https://lmvz-digitall.xyz/login-page.html"){
-  } else {
-      window.location.replace("https://lmvz-digitall.xyz/login-page.html");
