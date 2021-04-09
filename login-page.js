8cdeeb5d7c7ab5ff3f56c1e810fa13264e9e03ba
1290sdh <82150749+1290sdh@users.noreply.github.com>
2021-04-09 16:40:12 +0200

    Update login-page.js

--- login-page.js
+++ login-page.js
@@ -2,10 +2,6 @@ const loginForm = document.getElementById("login-form");
 const loginButton = document.getElementById("login-form-submit");
 const loginErrorMsg = document.getElementById("login-error-msg");
 
-window.onload = (event) => {
-  x.style.display = "none";
-};
-
 // When the login button is clicked, the following code is executed
 loginButton.addEventListener("click", (e) => {
     // Prevent the default submission of the form
