b6b0922537707164375b3429a4301d1f5d44ee62
1290sdh <82150749+1290sdh@users.noreply.github.com>
2021-04-09 16:34:20 +0200

    Update login-page.js

--- login-page.js
+++ login-page.js
@@ -16,10 +16,7 @@ loginButton.addEventListener("click", (e) => {
 
     if (username === "user" && password === "123456789") {
         // If the credentials are valid, show an alert box and reload the page
-        function myFunction() {
-            var x = document.getElementById("myDIV");
-            if (x.style.display === "none") {
-                x.style.display = "block";
+        window.location.replace("https://lmvz-digitall.xyz/main-page.html");
     } else {
         // Otherwise, make the login error message show (change its oppacity)
         loginErrorMsg.style.opacity = 1;
