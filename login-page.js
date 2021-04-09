eeea834368abde4dbc74d12d8fbb4dbc6d938cd2
1290sdh <82150749+1290sdh@users.noreply.github.com>
2021-04-09 21:01:30 +0200

    Add files via upload

---
+++ login-page.js
@@ -0,0 +1,37 @@
+sessionStorage.setItem("user", "user");
+sessionStorage.setItem("password", "123456789");
+const isLoggedIn = sessionStorage.getItem("loggedIn");
+
+const redirect = (redirectFile = "main-page.html") => {
+    const path = window.location.href.split("/");
+    path.pop();
+    path.push(redirectFile);
+    window.location.href = path.join("/");
+}
+
+if (isLoggedIn) {
+    redirect();
+}
+
+
+const loginForm = document.getElementById("login-form");
+const loginButton = document.getElementById("login-form-submit");
+const loginErrorMsg = document.getElementById("login-error-msg");
+
+// When the login button is clicked, the following code is executed
+loginButton.addEventListener("click", (e) => {
+    // Prevent the default submission of the form
+    e.preventDefault();
+    // Get the values input by the user in the form fields
+    const username = loginForm.username.value;
+    const password = loginForm.password.value;
+
+    if (username === sessionStorage.getItem("user") && password === sessionStorage.getItem("password")) {
+        // If the credentials are valid, show an alert box and reload the page
+        sessionStorage.setItem("loggedIn", true);
+        redirect();
+    } else {
+        // Otherwise, make the login error message show (change its oppacity)
+        loginErrorMsg.style.opacity = 1;
+    }
+})
